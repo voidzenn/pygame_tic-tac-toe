1b34a6ff3e7d5f3f4dafa2b4b3ed73669832b0e8
mike <mikesil121@gmail.com>
2023-06-21 15:34:18 +0800

    Created box class and created draw function

--- classes/box.py
+++ classes/box.py
@@ -2,24 +2,36 @@ import pygame
 
 SIZE = 100
 PADDING = 10
-BOX_X = 100
-BOX_Y = 100
 
 class Box:
-  def __init__(self, screen, size = SIZE, padding = PADDING, x = BOX_X, y = BOX_Y):
+  def __init__(self, screen, x = 100, y = 100, size = SIZE, padding = PADDING):
     self.box = pygame.Rect(x, y, size, size)
+    self.x = x
+    self.y = y
     self.screen = screen
     self.size = size
     self.padding = padding
-    self.x = 100
-    self.y = 100
+
+  def size(self):
+    return self.size
+
+  def x(self):
+    return self.x
+
+  def y(self):
+    return self.y
+
+  def padding(self):
+    return self.padding
 
   def draw(self):
     pygame.draw.rect(self.screen, (255, 0, 0), self.box)
 
   def is_hovered(self, mouse_position):
-    if (mouse_position[0] >= self.x and
-      mouse_position[0] <= (self.x + self.size - self.padding) and
-      mouse_position[1] >= self.y and
-      mouse_position[1] <= (self.y + self.size - self.padding)):
-        return (self.x, self.y)
+    x = self.x
+    y = self.y
+    if (mouse_position[0] >= x and
+      mouse_position[0] <= (x + self.size - self.padding) and
+      mouse_position[1] >= y and
+      mouse_position[1] <= (y + self.size - self.padding)):
+        return (x, y)
--- main.py
+++ main.py
@@ -8,23 +8,40 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 WINDOW_TITLE =  "Tic Tac Toe"
 
+box_positions = []
+
+def create_boxes():
+  for row in range(3):
+    for col in range(3):
+      x = col * (100 + 10)
+      y = row * (100 + 10)
+      box = Box(screen, x, y)
+      box.draw()
+      box_positions.append(box)
+
+def handle_click(mouse_pos):
+  for box in box_positions:
+    current_position = box.is_hovered(mouse_pos)
+
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 screen.fill(BACKGROUND_COLOR)
 
-b = Box(screen)
-b.draw()
+create_boxes()
 
-while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
+def run_game():
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      mouse_pos = pygame.mouse.get_pos()
-      print(b.is_hovered(mouse_pos))
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        mouse_pos = pygame.mouse.get_pos()
+        handle_click(mouse_pos)
 
-  pygame.display.update()
+    pygame.display.update()
+
+run_game()
