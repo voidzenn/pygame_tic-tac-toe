53620a3e607353b616747ba0ae72f199f4a536d7
mike <mikesil121@gmail.com>
2023-06-26 14:43:44 +0800

    Moved the variable initialization inside run_game function

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ WINDOW_TITLE =  "Tic Tac Toe"
 BOX_SIZE = 100
 BOX_PADDING = 5
 
-box_positions = []
-game = Game()
-
 def create_boxes():
+  box_positions = []
   total_size = BOX_SIZE + BOX_PADDING
   for row in range(3):
     for col in range(3):
@@ -26,6 +24,7 @@ def create_boxes():
       box = Box(screen, x, y)
       box.draw()
       box_positions.append(box)
+  return box_positions
 
 def load_circle(position):
   image = CircleImage(screen, position)
@@ -35,7 +34,7 @@ def load_cross(position):
   image = CrossImage(screen, position)
   image.display_centered(BOX_PADDING)
 
-def handle_image(position, index):
+def handle_image(position, index, game):
   selected_boxes = game.get_selected_boxes()
   if position not in selected_boxes:
     if game.is_current_turn_circle():
@@ -49,14 +48,11 @@ def handle_image(position, index):
 
     game.add_position(position)
 
-def handle_click(mouse_pos):
+def handle_click(mouse_pos, game, box_positions):
   for index, box in enumerate(box_positions):
     current_position = box.is_hovered(mouse_pos)
     if current_position:
-      handle_image(current_position, index)
-
-def initialize_game():
-  create_boxes()
+      handle_image(current_position, index, game)
 
 pygame.init()
 
@@ -64,10 +60,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 screen.fill(BACKGROUND_COLOR)
 
-text = Text(screen, (SCREEN_HEIGHT, SCREEN_WIDTH))
-
 def run_game():
-  initialize_game()
+  game = Game()
+  box_positions = create_boxes()
+  text = Text(screen, (SCREEN_HEIGHT, SCREEN_WIDTH))
 
   while True:
     for event in pygame.event.get():
@@ -80,7 +76,7 @@ def run_game():
           count = game.get_count_selected_boxes()
           if count < len(box_positions):
             mouse_pos = pygame.mouse.get_pos()
-            handle_click(mouse_pos)
+            handle_click(mouse_pos, game, box_positions)
 
       if event.type == pygame.MOUSEBUTTONUP:
         if game.is_circle_won() or game.is_cross_won():
